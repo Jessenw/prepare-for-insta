237b1802d4ab52244c12bfb00c0d7d13d101148d
Jesse Williams <jessenw3@gmail.com>
2020-09-29 22:15:00 +1300

    Add basic gui

--- main.py
+++ main.py
@@ -1,79 +1,96 @@
 from datetime import datetime
 from enum import Enum
-import os
 from pathlib import Path
 from PIL import Image
+import tkinter as tk
 
-WHITE = (255, 255, 255)
-PADDING = 500
+import os
 
-'''
-Directories
-'''
-SRC_FOLDER = "/Users/jesse/Desktop/src_images/"
-OUT_FOLDER = ""
-OUT_FOLDER_DEFAULT = "/Users/jesse/Documents/"
+class Application(tk.Frame):
+    WHITE = (255, 255, 255)
+    PADDING = 60
 
-class Orientation(Enum):
-    LANDSCAPE = 1
-    PORTRAIT = 2
+    '''
+    Directories
+    '''
+    SRC_FOLDER = "/Users/jesse/Desktop/fuckitywuckity/"
+    OUT_FOLDER = ""
+    OUT_FOLDER_DEFAULT = "/Users/jesse/Documents/"
 
-def main():
-    for _, _, files in os.walk(SRC_FOLDER):
-        for filename in files:
-            process_image(filename)
-            delete_src(SRC_FOLDER + filename)
+    class Orientation(Enum):
+        LANDSCAPE = 1
+        PORTRAIT = 2
 
-def process_image(filename):
-    print("Processing images...")
+    def process_start(self):
+        print("Processing images...")
+        for _, _, files in os.walk(self.SRC_FOLDER):
+            for filename in files:
+                self.process_image(filename)
+                self.delete_src(self.SRC_FOLDER + filename)
+        print("Done")
 
-    src = Image.open(SRC_FOLDER + filename)
-    src_size = src.size
+    def process_image(self, filename):
+        src = Image.open(self.SRC_FOLDER + filename)
+        src_size = src.size
 
-    # Determine whether the image is portrait/landscape
-    orientation = Orientation.LANDSCAPE
-    max_dimmension = src_size[0]
-    if src_size[0] < src_size[1]:
-        orientation = Orientation.PORTRAIT
-        max_dimmension = src_size[1]
+        # Determine whether the image is portrait/landscape
+        orientation = self.Orientation.LANDSCAPE
+        max_dimmension = src_size[0]
+        if src_size[0] < src_size[1]:
+            orientation = self.Orientation.PORTRAIT
+            max_dimmension = src_size[1]
 
-    out_width = max_dimmension
-    out_height = max_dimmension
-    if orientation == Orientation.LANDSCAPE:
-        out_width = out_width + PADDING
-    else:
-        out_height = out_height + PADDING
-    out_size = (out_width, out_height)
+        out_width = max_dimmension
+        out_height = max_dimmension
+        if orientation == self.Orientation.LANDSCAPE:
+            out_width = out_width + self.PADDING
+        else:
+            out_height = out_height + self.PADDING
+        out_size = (out_width, out_height)
 
-    out_img = Image.new("RGB", out_size, WHITE)
+        out_img = Image.new("RGB", out_size, self.WHITE)
 
-    mid_x = int((out_size[0] - src_size[0]) / 2)
-    mid_y = int((out_size[1] - src_size[1]) / 2)
-    out_img.paste(src, (mid_x, mid_y))
+        mid_x = int((out_size[0] - src_size[0]) / 2)
+        mid_y = int((out_size[1] - src_size[1]) / 2)
+        out_img.paste(src, (mid_x, mid_y))
 
-    save(src, out_img, filename)
+        self.save(src, out_img, filename)
 
+    def save(self, src, out, filename):
+        date = datetime.today().strftime('%Y-%m-%d')
 
-def save(src, out, filename):
-    date = datetime.today().strftime('%Y-%m-%d')
+        dir_raw = self.OUT_FOLDER_DEFAULT + date + "/raw/"
+        dir_processed = self.OUT_FOLDER_DEFAULT + date + "/processed/"
 
-    dir_raw = OUT_FOLDER_DEFAULT + date + "/raw/"
-    dir_processed = OUT_FOLDER_DEFAULT + date + "/processed/"
+        Path(dir_raw).mkdir(parents=True, exist_ok=True)
+        Path(dir_processed).mkdir(parents=True, exist_ok=True)
 
-    Path(dir_raw).mkdir(parents=True, exist_ok=True)
-    Path(dir_processed).mkdir(parents=True, exist_ok=True)
+        # write file
+        src.save(dir_raw + filename)
+        out.save(dir_processed + filename) 
 
-    # write file
-    src.save(dir_raw + filename)
-    out.save(dir_processed + filename)
+    def delete_src(self, filename):
+        os.remove(filename)
 
-def delete_src(filename):
-    os.remove(filename)
+        # Delete folder if empty
+        if not os.listdir(self.SRC_FOLDER):
+            os.rmdir(self.SRC_FOLDER)
 
-    # Delete folder if empty
-    if not os.listdir(SRC_FOLDER):
-        os.rmdir(SRC_FOLDER)
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
+    def create_widgets(self):
+        self.start = tk.Button(self)
+        self.start["text"] = "Start"
+        self.start["command"] = self.process_start
+        self.start.pack(side="top")
 
-if __name__ == '__main__':
-    main()
+        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
+        self.quit.pack(side="bottom")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
