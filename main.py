f8097dd4fc051de954691780d2a72c16f10faded
Jesse Williams <jessenw3@gmail.com>
2020-09-30 08:50:59 +1300

    support arguments

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
+import argparse
+import os
 from datetime import datetime
 from enum import Enum
-import os
 from pathlib import Path
 from PIL import Image
 
 WHITE = (255, 255, 255)
-PADDING = 500
 
 '''
 Directories
 '''
-SRC_FOLDER = "/Users/jesse/Desktop/src_images/"
-OUT_FOLDER = ""
-OUT_FOLDER_DEFAULT = "/Users/jesse/Documents/"
+src_dir = "/Users/jesse/Desktop/src_images/"
+dst_dir = "/Users/jesse/Documents/"
+
+padding = 0
 
 class Orientation(Enum):
     LANDSCAPE = 1
     PORTRAIT = 2
 
 def main():
-    for _, _, files in os.walk(SRC_FOLDER):
+    print("Processing images...")
+    for _, _, files in os.walk(src_dir):
         for filename in files:
             process_image(filename)
-            delete_src(SRC_FOLDER + filename)
+            # delete_src(src_dir + filename)
 
 def process_image(filename):
-    print("Processing images...")
-
-    src = Image.open(SRC_FOLDER + filename)
+    src = Image.open(src_dir + filename)
     src_size = src.size
 
     # Determine whether the image is portrait/landscape
@@ -40,9 +40,9 @@ def process_image(filename):
     out_width = max_dimmension
     out_height = max_dimmension
     if orientation == Orientation.LANDSCAPE:
-        out_width = out_width + PADDING
+        out_width = out_width + padding
     else:
-        out_height = out_height + PADDING
+        out_height = out_height + padding
     out_size = (out_width, out_height)
 
     out_img = Image.new("RGB", out_size, WHITE)
@@ -53,12 +53,11 @@ def process_image(filename):
 
     save(src, out_img, filename)
 
-
 def save(src, out, filename):
     date = datetime.today().strftime('%Y-%m-%d')
 
-    dir_raw = OUT_FOLDER_DEFAULT + date + "/raw/"
-    dir_processed = OUT_FOLDER_DEFAULT + date + "/processed/"
+    dir_raw = dst_dir + date + "/raw/"
+    dir_processed = dst_dir + date + "/processed/"
 
     Path(dir_raw).mkdir(parents=True, exist_ok=True)
     Path(dir_processed).mkdir(parents=True, exist_ok=True)
@@ -71,9 +70,30 @@ def delete_src(filename):
     os.remove(filename)
 
     # Delete folder if empty
-    if not os.listdir(SRC_FOLDER):
-        os.rmdir(SRC_FOLDER)
-
+    if not os.listdir(src_dir):
+        os.rmdir(src_dir)
 
 if __name__ == '__main__':
+    # Setup arguments labels
+    parser = argparse.ArgumentParser()
+    parser.add_argument('src', help="The source directory of images to be processed")
+    parser.add_argument('dst', help="The destination directory for images to be saved")
+    parser.add_argument('padding', help="Adds padding beyond the frame of 1:1 frame of the image. Default is no padding")
+    args = parser.parse_args()
+
+    src_dir = args.src.split("=")[1]
+    dst_dir = args.dst.split("=")[1]
+    padding = int(args.padding.split("=")[1])
+
+    # Append '/' to directory paths if they dont exist
+    if not src_dir[-1] == '/':
+        src_dir = src_dir + '/'
+    if not dst_dir[-1] == '/':
+        dst_dir = dst_dir + '/'
+
+    # Check if there are images available to process
+    if not os.listdir(src_dir):
+        print("Aborting... The provided source folder is empty")
+        exit()
+
     main()
